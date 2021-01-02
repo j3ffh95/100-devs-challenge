d4932324100adc843138d8c73348b1a412dc34b9
j3ffh95 <j3ffh95@gmail.com>
2021-01-02 15:12:05 -0500

    Created function five

--- js/main.js
+++ js/main.js
@@ -13,10 +13,21 @@ function funcThree(num1, num2, num3) {
 }
 //---Medium
 //create a function that takes in three numbers. Add the first two numbers and return the remainder of dividing the sum of the first two numbers by the third number
-function funcFive(num1, num2, num3) {
+function funcFour(num1, num2, num3) {
   let addedNumbers = num1 + num2;
   return addedNumbers % num3;
 }
 
 //---Hard
 //create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
+function funcFive(num1, num2, num3, num4) {
+  let multiplyNum = num1 * num2;
+  if(multiplyNum > 100) {
+    console.log(num3 + num4)
+  } elseif(multiplyNum < 100) {
+    console.log(num3 - num4)
+  } elseif(multiplyNum === 100) {
+    let multiplyNumTwo = num1 * num2 * num3;
+    alert(multiplyNumTwo % num4)
+  }
+}
